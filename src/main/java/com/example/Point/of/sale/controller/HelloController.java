49bbbc8cffee4b9b383c4b19cd6a71ee3f8b1c6a
yassminzouani <zouaniyassmine@gmail.com>
2026-01-01 12:41:19 +0100

    tp30

---
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.example.Point.of.sale.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+   @GetMapping
+public String hello() {
+    return "Hello from New test0001000 :)";
+}
+
+    @GetMapping("/user")
+    public String getUsers(){
+        return "Users";
+    }
+
+
+    @GetMapping("/presentation")
+    public String getPresentation(){
+        return "presentation";
+    }
+}
